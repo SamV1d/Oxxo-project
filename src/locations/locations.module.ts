ecabec5ec8adaa0857bbc3c7f6b21c5e2a2a79b9
SamV1d <evidal17@alumnos.uaq.mx>
2025-03-08 23:24:19 -0600

    Initial config for locations route

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LocationsService } from './locations.service';
 import { LocationsController } from './locations.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  controllers: [LocationsController],
+  imports:[TypeOrmModule.forFeature([Location])]
+  ,controllers: [LocationsController],
   providers: [LocationsService],
 })
 export class LocationsModule {}
