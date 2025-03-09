411d06577b79300238271eeb1c85937a5b2d635f
SamV1d <evidal17@alumnos.uaq.mx>
2025-03-08 22:56:09 -0600

    Created three new resources to expand backend capibilities

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
 import { ProvidersModule } from './providers/providers.module';
+import { ManagersModule } from './managers/managers.module';
+import { LocationsModule } from './locations/locations.module';
+import { RegionsModule } from './regions/regions.module';
 
 
 @Module({
@@ -20,7 +23,7 @@ import { ProvidersModule } from './providers/providers.module';
       autoLoadEntities: true,
       synchronize: true,
   }
-  ),EmployeesModule, ProductsModule, ProvidersModule],
+  ),EmployeesModule, ProductsModule, ProvidersModule, ManagersModule, LocationsModule, RegionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
